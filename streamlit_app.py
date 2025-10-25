d2843d29806e59a74eac2c95ff8e45ef18186229
SHAWN TAN <tanernhong@gmail.com>
2025-10-25 22:41:12 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,13 @@ uploaded_file = st.file_uploader("Pick a file.")
 if uploaded_file is not None:
 
     try:
-        df = pd.read_excel(uploaded_file, skiprows=6)
+        with open(uploaded_file, 'rb') as f:
+            first_bytes = f.read(4)
+            if first_bytes==b'PK\x03\x04':
+                df = pd.read_excel(uploaded_file, skiprows=6)
+            else:
+                df = pd.read_csv(uploaded_file, skiprows=6)
+
         start = df.index[df['Time']=='Deals'].tolist()[0]
         end = df.index[df['Time']=='Open Positions'].tolist()[0]
         df = pd.read_excel(uploaded_file, skiprows=start+8, nrows=end-start-3)
