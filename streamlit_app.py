803ef4bfa6c7fa5bd2bd9ceb027d8918d49b5151
SHAWN TAN <tanernhong@gmail.com>
2025-10-25 21:45:56 +0800

    Added download_button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import datetime
 
 st.set_page_config(
     page_title="Strategy analyser",
     page_icon=":chart_with_upwards_trend:"
 )
 
-file = st.file_uploader("Pick a file.")
+df = st.file_uploader("Pick a file.")
+
+datetime_stamp = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+st.download_button(
+    label="Download data as CSV.",
+    data=df,
+    file_name=f'processed_report_{datetime_stamp}.csv',
+    mime='text/csv'
+)
