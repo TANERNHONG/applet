c22efdfc902a66e795e3de33fdff7eacb8978694
SHAWN TAN <tanernhong@gmail.com>
2025-10-25 21:38:04 +0800

    Testing by making repository private.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(
+    page_title="Strategy analyser",
+    page_icon=":chart_with_upwards_trend:"
+)
+
 file = st.file_uploader("Pick a file.")
