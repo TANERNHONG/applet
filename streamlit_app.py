c22d1cb8b7d7b4b2c7df2c7d033dc041dbfcbde1
SHAWN TAN <tanernhong@gmail.com>
2025-10-25 22:18:11 +0800

    No comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,37 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from io import BytesIO
 
 st.set_page_config(
     page_title="Strategy analyser",
     page_icon=":chart_with_upwards_trend:"
 )
 
-file = st.file_uploader("Pick a file.")
+uploaded_file = st.file_uploader("Pick a file.")
 
-if file:
-    df = pd.read_excel(file)
+if uploaded_file is not None:
 
-    @st.cache_data
-    def convert_for_download(df):
-        return df.to_csv().encode("utf-8")
+    file_bytes = BytesIO(uploaded_file.read())
+    try:
+        df = pd.read_excel(file_bytes)
 
-    datetime_stamp = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+        @st.cache_data
+        def convert_for_download(df):
+            return df.to_csv().encode("utf-8")
 
-    if st.button("Process files."):
-        csv = convert_for_download(df)
-        st.download_button(
-            label="Download output as CSV.",
-            data=csv,
-            file_name=f'processed_report_{datetime_stamp}.csv',
-            on_click="ignore",
-            type="primary",
-            mime='text/csv',
-            icon=":material/download:",
-        )
+        datetime_stamp = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 
-else:
-    st.warning("The file uploaded is not readable. Try again.")
+        if st.button("Process files."):
+            csv = convert_for_download(df)
+            st.download_button(
+                label="Download output as CSV.",
+                data=csv,
+                file_name=f'processed_report_{datetime_stamp}.csv',
+                on_click="ignore",
+                type="primary",
+                mime='text/csv',
+                icon=":material/download:",
+            )
+    except Exception as e:
+        st.warning("The file uploaded is not readable. Try again.")
