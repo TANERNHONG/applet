9ee05083558c663620b79e4e2eee5669571b0294
SHAWN TAN <tanernhong@gmail.com>
2025-10-25 22:50:28 +0800

    Added core report processing logic.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import numpy as np
 from datetime import datetime
 from io import BytesIO
 import openpyxl
+import re
 
 class Trade:
     def __init__(self, symbol, deal_in, deal_out, volume, time_in, time_out, status, strategy):
@@ -46,7 +47,70 @@ if uploaded_file is not None:
         df = pd.read_excel(uploaded_file, skiprows=start+8, nrows=end-start-3)
         df = df.dropna(subset=['Symbol'])
 
-        st.dataframe(df)
+        list_of_trades = []
+        for index, row in df.iterrows():
+            if row['Direction'] == 'in':
+                datetime_str_in = row['Time']
+                datetime_obj_in = datetime.strptime(datetime_str_in, '%Y.%m.%d %H:%M:%S')
+
+                pattern = r'Taken by (\w+) v\d+\.\d+'
+                extracted_text = ''
+                match = re.search(pattern, row['Comment'])
+                if match:
+                    extracted_text = match.group(1)
+                    # print(extracted_text)
+                
+                elif row['Comment'] == 'SALAD':
+                    extracted_text = 'SALAD'
+
+                found_out = False
+                i = 0
+                out = []
+                while found_out == False:
+
+                    if index+i == len(df):
+                        found_out = True
+                        # print(f"There is no corresponding out trade for this trade")
+                        break
+
+                    check_row = df.iloc[index + i]
+
+                    if check_row['Symbol'] == row['Symbol'] and \
+                        ('[tp' in check_row['Comment'] or '[sl' in check_row['Comment']) and \
+                            check_row['Volume'] == row['Volume'] and \
+                                ((row['Type'] == 'buy' and check_row['Type'] == 'sell') or (row['Type'] == 'sell' and check_row['Type'] == 'buy')):
+                        out.append(check_row)
+                        found_out = True
+                        # print(f"Corresponding out trade found.")
+                    else:
+                        i = i + 1
+                
+                if out != []:
+                    datetime_str_out = out[0]['Time']
+                    datetime_obj_out = datetime.strptime(datetime_str_out, '%Y.%m.%d %H:%M:%S')
+                    time_elapsed = datetime_obj_out - datetime_obj_in
+                    status = ''
+
+                    if '[tp' in check_row['Comment']:
+                        status = 'TP'
+                    elif '[sl' in check_row['Comment']:
+                        status = 'SL'
+
+                    newrow = {
+                        'deal_in': row,
+                        'deal_out': check_row,
+                        'symbol': row['Symbol'],
+                        'volume': row['Volume'],
+                        'strategy': extracted_text,
+                        'time_in': datetime_obj_in,
+                        'time_out': datetime_obj_out,
+                        'status': status
+                    }
+
+                    list_of_trades.append(newrow)
+
+        trades_df = pd.DataFrame(list_of_trades)
+        st.dataframe(trades_df)
 
         @st.cache_data
         def convert_for_download(df):
@@ -55,7 +119,7 @@ if uploaded_file is not None:
         datetime_stamp = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 
         if st.button("Process files."):
-            csv = convert_for_download(df)
+            csv = convert_for_download(trades_df)
             st.download_button(
                 label="Download output as CSV.",
                 data=csv,
